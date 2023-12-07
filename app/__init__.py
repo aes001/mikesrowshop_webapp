2fc3677dd90c96a5795d72c0c08dd5b1aee808dc
aes <edo.sunardi@gmail.com>
2023-12-07 01:25:04 +0000

    Disabled dotenv because it's causing problems in the server

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@
 # Design of this app is inspired by the Adobe Spectrum design system
 
 from flask import Flask
-# from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
@@ -13,7 +12,7 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
 # Load environment variables from .env file (used for SECRET_KEY)
-# load_dotenv()
+
 
 # If I don't include this, I get an error when running db_create.py
 # It's about the app running outside of the context of the application instance
